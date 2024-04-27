f1f0c4bdf265b0b8d58bd6990e36a1785b3a3511
anderDaZu <anderda-96@hotmail.com>
2024-04-27 09:51:10 -0500

    Componentes como plantilla

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-    
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+{{-- Componentes como plantillas --}}
+<x-app-layout>
+    <x-slot name="title">
+        Posts
+    </x-slot-title>
 
     {{-- Componentes anonimos --}}
     <x-container class="mt-4" width="4xl">
@@ -279,5 +273,4 @@
             // Otros datos
         };
     </script> --}}
-</body>
-</html>
+</x-app-layout>
