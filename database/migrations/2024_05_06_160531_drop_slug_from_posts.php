489a18d37689661e4b771afc52402696508e6d5b
anderDaZu <anderda-96@hotmail.com>
2024-05-06 11:12:15 -0500

    Eliminar columna de tabla desde migración

---
+++ database/migrations/2024_05_06_160531_drop_slug_from_posts.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Eleminar columna de una tabla
+            // php artisan make:migration drop_slug_from_posts_table
+            $table->dropColumn('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('slug')
+                ->unique()
+                ->after('main_title');
+        });
+    }
+};
