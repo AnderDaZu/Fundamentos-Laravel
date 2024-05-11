f4479ac511e9ddc9d06fbbe498a02a3b5ad8ef22
anderDaZu <anderda-96@hotmail.com>
2024-05-10 22:07:43 -0500

    Eliminar llaves foraneas

---
+++ database/migrations/2024_05_11_025839_alter_to_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // $table->dropForeign('posts_user_id_foreign');
+            // ☝️ = 👇
+            $table->dropForeign(['user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // no se debe usar foreignId y constrained
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+};
